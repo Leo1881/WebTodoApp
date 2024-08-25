13f4c763f6b5ae3b54850c5b324da088b5af9cfb
Lee <leaveil.johnson1881@gmail.com>
2024-08-25 10:41:15 +0200

    Implement add feature

--- web.py
+++ web.py
@@ -3,13 +3,23 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("My list of things to do.")
 #st.write("This is for simple text")
+
 
 incrementor = 1
 for todo in todos:
     st.checkbox(todo, key=incrementor)
     incrementor = incrementor + 1
 
-st.text_input(label="", placeholder="Add something to do: ")
+st.text_input(label="", placeholder="Add something to do: ", on_change=add_todo, key="new_todo")
+
+st.session_state
